dbc8a6a5530a98f5134449f63d8a989d837320fa
drabi-he <drabi-he@debian-BULLSEYE-live-builder-AMD64>
2023-06-19 12:46:15 +0100

    Fully functional

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,70 +1,64 @@
-import { publicProcedure, router } from '../trpc';
-import prisma from '../common/prisma';
-import { z } from 'zod';
-import { generateAccessToken, isEmail, isPassword, verifyToken } from '../utils';
-import { compareSync } from 'bcryptjs';
-import { TRPCError } from '@trpc/server';
+import { publicProcedure, router } from "../trpc";
+import prisma from "../common/prisma";
+import { z } from "zod";
+import {
+  generateAccessToken,
+  isBetween,
+  isPassword,
+  verifyToken,
+} from "../utils";
+import { compareSync } from "bcryptjs";
+import { TRPCError } from "@trpc/server";
 
 export const loginSchema = z.object({
-	email: isEmail(),
-	password: isPassword(),
+  username: isBetween(4, 20),
+  password: isPassword(),
 });
 
 export const logoutSchema = z.object({
-	accessToken: z.string(),
+  accessToken: z.string(),
 });
 
-
 export const verifySchema = z.object({
-	accessToken: z.string(),
+  accessToken: z.string(),
 });
 
 const authRouter = router({
-	login: publicProcedure
-		.input(loginSchema)
-		.mutation(async (opts) => {
+  login: publicProcedure.input(loginSchema).mutation(async (opts) => {
+    const { username, password } = loginSchema.parse(opts.input);
 
-			const { email, password } = loginSchema.parse(opts.input);
+    const user = await prisma.user.findUnique({ where: { username } });
 
-			const user = await prisma.user.findUnique({ where: { email } });
+    if (!user || !compareSync(password, user.password))
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+        message: "Invalid username or password",
+      });
 
-			if (!user || !compareSync(password, user.password))
-				throw new TRPCError({
-					code: 'UNAUTHORIZED',
-					message: 'Invalid email or password',
-				})
+    const accessToken = generateAccessToken({ id: user.id });
+    await prisma.user.update({ where: { id: user.id }, data: { accessToken } });
+    return { accessToken };
+  }),
 
-			const accessToken = generateAccessToken({ id: user.id, });
-			await prisma.user.update({ where: { id: user.id }, data: { accessToken } });
-			return { accessToken };
-		}),
+  logout: publicProcedure.input(logoutSchema).mutation(async (opts) => {
+    const { accessToken } = logoutSchema.parse(opts.input);
+    const { id } = await verifyToken(accessToken);
+    await prisma.user.update({ where: { id }, data: { accessToken: null } });
+    return { success: true };
+  }),
 
-	logout: publicProcedure
-		.input(logoutSchema)
-		.mutation(async (opts) => {
-			const { accessToken } = logoutSchema.parse(opts.input);
-			const { id } = await verifyToken(accessToken);
-			await prisma.user.update({ where: { id }, data: { accessToken: null } });
-			return { success: true };
-		}),
-
-	verify: publicProcedure
-		.input(verifySchema)
-		.mutation(async (opts) => {
-			const { accessToken } = opts.input;
-			const { id } = await verifyToken(accessToken);
-			const user = await prisma.user.findUnique({ where: { id } });
-			if (!user) {
-				throw new TRPCError({
-					code: 'UNAUTHORIZED',
-					message: 'Invalid access token',
-				})
-			}
-			return user;
-		})
+  verify: publicProcedure.input(verifySchema).mutation(async (opts) => {
+    const { accessToken } = opts.input;
+    const { id } = await verifyToken(accessToken);
+    const user = await prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+        message: "Invalid access token",
+      });
+    }
+    return user;
+  }),
 });
 
-
-
 export default authRouter;
-
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,81 +1,67 @@
-import { adminProcedure as procedure, router } from '../trpc';
-import prisma from '../common/prisma';
-import { z } from 'zod';
-import { isBetween, isEmail, isId } from '../utils';
-import bcrypt from 'bcryptjs';
-
-
+import { adminProcedure as procedure, router } from "../trpc";
+import prisma from "../common/prisma";
+import { z } from "zod";
+import { isBetween, isEmail, isId } from "../utils";
+import bcrypt from "bcryptjs";
 
 export const createUserSchema = z.object({
-	id: z.number().optional(),
-	firstName: isBetween(2, 50),
-	lastName: isBetween(2, 50),
-	email: isEmail(),
-	password: z.string().min(8),
-	role: z.enum(['ADMIN', 'USER']).optional(),
+  id: z.number().optional(),
+  firstName: isBetween(2, 50),
+  lastName: isBetween(2, 50),
+  username: isBetween(4, 10),
+  password: z.string().min(8),
+  role: z.enum(["ADMIN", "USER"]).optional(),
 });
 
 export const updateUserSchema = createUserSchema.partial();
 
 const userRouter = router({
+  findMany: procedure.query(async ({ ctx }) => {
+    const { user } = ctx;
+    return await prisma.user.findMany({
+      where: { id: { not: user.id } },
+    });
+  }),
 
-	findMany: procedure
-		.query(async ({ ctx }) => {
-			const { user } = ctx;
-			return await prisma.user.findMany({
-				where: { id: { not: user.id } }
-			});
-		}),
+  findUnique: procedure.input(isId()).query(async (opts) => {
+    const { input } = opts;
+    return await prisma.user.findUnique({ where: { id: input } });
+  }),
 
-	findUnique: procedure
-		.input(isId())
-		.query(async (opts) => {
-			const { input } = opts;
-			return await prisma.user.findUnique({ where: { id: input } });
-		}),
+  create: procedure.input(createUserSchema).mutation(async (opts) => {
+    const { input } = opts;
+    const { password, ...rest } = input;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return await prisma.user.create({
+      data: {
+        ...rest,
+        password: hashedPassword,
+      },
+    });
+  }),
 
-	create: procedure
-		.input(createUserSchema)
-		.mutation(async (opts) => {
-			const { input } = opts;
-			const { password, ...rest } = input;
-			const salt = await bcrypt.genSalt(10);
-			const hashedPassword = await bcrypt.hash(password, salt);
-			return await prisma.user.create({
-				data: {
-					...rest,
-					password: hashedPassword,
-				}
-			});
-		}),
+  update: procedure
+    .input(
+      z.object({
+        id: isId(),
+        data: updateUserSchema,
+      })
+    )
+    .mutation(async (opts) => {
+      const { input } = opts;
+      const { id, data } = input;
+      return await prisma.user.update({ where: { id }, data });
+    }),
 
-	update: procedure
-		.input(z.object({
-			id: isId(),
-			data: updateUserSchema,
-		}))
-		.mutation(async (opts) => {
-			const { input } = opts;
-			const { id, data } = input;
-			return await prisma.user.update({ where: { id }, data });
-		}),
+  delete: procedure.input(isId()).mutation(async (opts) => {
+    const { input } = opts;
+    return await prisma.user.delete({ where: { id: input } });
+  }),
 
-	delete: procedure
-		.input(isId())
-		.mutation(async (opts) => {
-			const { input } = opts;
-			return await prisma.user.delete({ where: { id: input } });
-		}),
-
-	deleteMany: procedure
-		.mutation(async () => {
-			return await prisma.user.deleteMany();
-		}),
-
-
-},);
-
-
+  deleteMany: procedure.mutation(async () => {
+    return await prisma.user.deleteMany();
+  }),
+});
 
 export default userRouter;
-
