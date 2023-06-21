fbd1727e5014209b1b73a9df6e48ae8cda8791d3
oussamajamil <55845632+oussamajamil@users.noreply.github.com>
2023-06-21 13:02:18 +0100

    add edit code

--- frontend/src/pages/main/device-profile/index.tsx
+++ frontend/src/pages/main/device-profile/index.tsx
@@ -9,7 +9,6 @@ import {
 } from "@material-tailwind/react";
 import {
   PencilIcon,
-  PlusCircleIcon,
   TrashIcon,
 } from "@heroicons/react/24/outline";
 import DataGrid, { Column } from "../../../components/data-grid";
@@ -24,6 +23,7 @@ import {
   Protocol,
   State,
 } from "../../../utils/types.ts";
+import { FaPlus } from "react-icons/fa";
 
 const defaultData: Data = {
   name: "",
@@ -260,7 +260,7 @@ export function DeviceProfilePage() {
               className="flex justify-center items-center gap-3 rounded-full p-0 h-10  md:h-12 aspect-square "
               onClick={() => setData(defaultData)}
             >
-              <PlusCircleIcon strokeWidth={2} className="w-7 md:w-8" />
+              <FaPlus className="text-xl" />
             </Button>
             <Pagination
               className="mt-auto md:p-3 lg:p-4 ml-auto"
